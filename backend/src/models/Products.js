f908b6773d2251d223fa9b66240dfd7f59fb0580
EdeAmaya <edenilsonamaya10@gmail.com>
2025-09-01 23:36:33 -0600

    CRUD Productos

--- backend/src/models/Products.js
+++ backend/src/models/Products.js
@@ -9,29 +9,28 @@
 import { Schema, model } from "mongoose";
 
 const productsSchema = new Schema(
-  {
-    name: {
-      type: Number,
-      require: true,
-    },
-    desciption: {
-      type: String,
-    },
-    price: {
-      type: Number,
-      require: true,
-      min: 0,
-    },
-    stock: {
-      type: Number,
-      require: true,
-      min: 0,
-    },
-  },
-  {
-    timestamps: true,
-    strict: false,
-  }
-);
+  {     
+    name: {       
+      type: String,        
+      required: true,      
+    },     
+    description: {         
+      type: String,     
+    },     
+    price: {       
+      type: Number,       
+      required: true,     
+    },     
+    stock: {       
+      type: Number,       
+      required: true,      
+      min: 0,     
+    },   
+  },   
+  {     
+    timestamps: true,     
+    strict: false,   
+  } 
+);  
 
 export default model("Products", productsSchema);
